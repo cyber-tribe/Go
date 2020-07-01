2d5c4b513762d67709f4c7ef3fbf39724c8c51f1
cyber-tribe <cybertribe.nks8@gmail.com>
2020-07-02 01:58:22 +0900

    編集ページの追加

--- MyWiki/app.go
+++ MyWiki/app.go
@@ -8,13 +8,24 @@ import (
 
 func main() {
 	http.HandleFunc("/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
+	// 画面を描画する関数
 	title := r.URL.Path[6:]
 	p, _ := loadPage(title)
 	t, _ := template.ParseFiles("view.html")
+	t.Execute(w, p)
+}
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[6:]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title} // 空のPage構造体を作成
+	}
+	t, _ := template.ParseFiles("edit.html")
 	t.Execute(w, p)
 }
 
